2dbc1b3b992509b2fb467b785931e7442a4c56c0
Will Snavely <will.snavely@protonmail.com>
2020-10-20 01:04:53 -0400

    Adding docs and error messages to spider class

--- city_scrapers/spiders/monroeville_public_meetings.py
+++ city_scrapers/spiders/monroeville_public_meetings.py
@@ -1,3 +1,20 @@
+"""A Google Calendar spider for Monroevile public meetings
+
+This spider scrapers the Google Calendar provided by the 
+Municipality of Monroeville at this link:
+    https://www.monroeville.pa.us/calendar.htm
+
+The ID for this calendar, determined by inspecting
+network requests is:
+    municipalityofmonroeville@gmail.com
+
+To use this spider, a Google API key must be stored in the
+environment variable, "GOOGLE_API_KEY", e.g.
+
+export GOOGLE_API_KEY=your_api_key
+scrapy crawl monroeville_public_meetings
+"""
+
 import os
 from datetime import datetime, timedelta
 from typing import Optional
@@ -17,6 +34,11 @@ class MonroevillePublicMeetings(GoogleCalendarSpider):
     def __init__(self, *args, **kwargs):
         time_min = (datetime.utcnow() - timedelta(days=180)).isoformat() + "Z"
         time_max = (datetime.utcnow() + timedelta(days=180)).isoformat() + "Z"
+
+        if "GOOGLE_API_KEY" not in os.environ:
+            raise RuntimeError(
+                    "This spider requires a Google API key to be stored " 
+                    "in the environment variable 'GOOGLE_API_KEY'")
         google_api_key = os.environ["GOOGLE_API_KEY"] 
         request_params = {
             "singleEvents": True,
